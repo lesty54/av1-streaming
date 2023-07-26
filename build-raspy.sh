ee49f9ba6a411bb8682d2feb3c211f0ca0d3ae01
lesty54 <94396668+lesty54@users.noreply.github.com>
2023-07-26 16:51:40 +0200

    Add files via upload
    Check and install if cargo is not installed.
    cargo error

--- build-raspy.sh
+++ build-raspy.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -e
 
-USER=ubuntu
-PI_IP=192.168.18.39
+USER=lucas
+PI_IP=172.20.10.3
 TARGET=aarch64-unknown-linux-gnu
 
 sudo apt update
 sudo apt install -y libclang-dev libv4l-dev
+
+# Check if cargo is installed
+if ! command -v cargo &> /dev/null
+then
+    echo "Cargo is not installed, proceeding to install it..."
+    # Install rustup, Rust's version and toolchain manager
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # Add cargo to path
+    source $HOME/.cargo/env
+    echo "Cargo has been installed!"
+else
+    echo "Cargo is already installed!"
+fi
 
 # build binary
 cargo build --release --target $TARGET
@@ -13,3 +26,4 @@ cargo build --release --target $TARGET
 # upload binary
 ssh-copy-id $USER@$PI_IP
 scp -r ./target/$TARGET/release/video-streaming $USER@$PI_IP:/tmp/
+ 
